2550b1a9e5d3d2ff0d01172480760e83f1380c07
CreateSong <thdcodbs1@naver.com>
2023-09-11 17:33:46 +0900

    Feature: 노마드코더JS 'PadStart'

--- momentum/js/clock.js
+++ momentum/js/clock.js
@@ -4,10 +4,14 @@
 
 const clock = document.querySelector("h2#clock");
 
-
+// "1".padStart(2,"0")
+// 해석: 길이가 1개인 문자열이 있다고 가정할 때, padStart()를 써서 string("1)의 앞쪽 시작 부분에 padding("0")을 추가하여 문자열 길이가 2가 됨
 function getClock() {
   const date = new Date();
-  clock.innerText = (`${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`)
+  const hours = String(date.getHours()).padStart(2, "0");
+  const minutes = String(date.getMinutes()).padStart(2, "0");
+  const seconds = String(date.getSeconds()).padStart(2, "0");
+  clock.innerText = (`${hours}:${minutes}:${seconds}`)
 }
 
 // setInterval은 2개의 argument를 받음. 첫 번째는 실행할 함수, 두 번째는 호출되는 함수 간격을 몇 ms(milliseconds)로 할지 작성
